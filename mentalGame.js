27cc7568bd1f9b7a6d95c1007948be5b52830ef2
AngiMangy <kickmango16@gmail.com>
2025-09-18 08:08:58 -0400

    Completed Level One of challenge

--- mentalGame.js
+++ mentalGame.js
@@ -7,8 +7,9 @@ let question = 1;
 let score = 0;
 let num1 = 1;
 let num2 = 3;
+let strike = 0;
 
-// Level One Solution
+// Max Score Easy
 function maxScore1(){
     while(question <= 20){
         answer = Number(prompt(`${question}) ${num1}+${num2} = `));
@@ -33,3 +34,24 @@ function maxScore1(){
         console.log(`Your final score is ${score}/200`);
     }
 }
+// Three Out, Easy
+function threeOut1(){
+    while(strike < 3){
+        answer = Number(prompt(`${question}) ${num1}+${num2} = `));
+        if(isNaN(answer)){
+            strike = strike + 1
+            console.log(`invalid input, thats strike ${strike}/3,`)
+            }
+        else if(answer === num1+num2){
+            console.log(`Correct :)`);
+        }
+        else{
+            strike = strike + 1
+            console.log(`You Dumb? That's strike ${strike}/3`);
+        }
+        num1 = num1 + 1;
+        num2 = num2 + 2;
+        question = question + 1;
+    }
+}
+threeOut1()
